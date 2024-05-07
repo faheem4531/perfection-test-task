20cdf64f844c1e042d9c6ec6421b15cdbd3cf9f1
faheem4531 <faheem4531@gmail.com>
2024-05-08 02:19:55 +0500

    Basic modal completion

--- src/app/_components/Modals/BasicModal.tsx
+++ src/app/_components/Modals/BasicModal.tsx
@@ -1,30 +1,44 @@
 'use client'
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Modal } from 'antd';
-import "./styles.scss"
+import Image from 'next/image';
 
-const BasicModal: React.FC = () => {
-  const [open, setOpen] = useState(false);
+import styles from "./styles.module.scss"
+import Warning from '@/app/_assets/svgs/modal-warning-icon.svg';
+import Check from '@/app/_assets/svgs/modal-check-icon.svg';
+
+interface BasicModalProps {
+  icon: string;
+  title: string;
+  onClick: () => void;
+  open: boolean;
+  onClose: () => void
+  cancleBtn: boolean
+}
+
+const BasicModal: React.FC<BasicModalProps> = ({ icon, title, onClick, open, onClose, cancleBtn }) => {
 
   return (
-    <>
-      <Button type="primary" onClick={() => setOpen(true)}>
-        Open Modal of 1000px width
-      </Button>
-      <Modal
-        title="Modal 1000px width"
-        centered
-        open={open}
-        onOk={() => setOpen(false)}
-        onCancel={() => setOpen(false)}
-        width={400}
-        footer={null}
-
-      >
-        <p>some contents...</p>
-      </Modal>
-    </>
+    <Modal
+      centered
+      open={open}
+      onOk={onClose}
+      onCancel={onClose}
+      width={400}
+      footer={null}
+    >
+      <Image src={icon === 'alert' ? Warning : Check} alt="modal icon" />
+      <p className={styles.modalText}>{title}</p>
+      <div className={styles.btnContainer}>
+        <Button className={styles.btnCheck} onClick={onClick} >
+          확인
+        </Button>
+        {cancleBtn && <Button className={styles.btnCancle} onClick={onClose} >
+          취소
+        </Button>}
+      </div>
+    </Modal>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
+'use client'
+
+import React, { useState } from 'react';
+
 import Image from "next/image";
 import BasicModal from './_components/Modals/BasicModal';
+import { Button, Modal } from 'antd';
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
+
   return (
     <div >
-      <BasicModal />
+      <Button type="primary" onClick={() => setOpen(true)}>
+        Open Modal of 1000px width
+      </Button>
+      {/* <BasicModal
+        cancleBtn={true}
+        icon="alert"
+        title={"투자유형을 변경하시겠습니까?"}
+        onClick={() => { }}
+        open={open}
+        onClose={() => setOpen(false)}
+      /> */}
     </div>
   );
 }
