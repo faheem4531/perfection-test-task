a55eed7912911596ad329b3883a92da5993c8d54
faheem4531 <faheem4531@gmail.com>
2024-05-07 23:33:53 +0500

    initializing project

---
+++ src/app/_components/Modals/BasicModal.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import React, { useState } from 'react';
+import { Button, Modal } from 'antd';
+import "./styles.scss"
+
+const BasicModal: React.FC = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button type="primary" onClick={() => setOpen(true)}>
+        Open Modal of 1000px width
+      </Button>
+      <Modal
+        title="Modal 1000px width"
+        centered
+        open={open}
+        onOk={() => setOpen(false)}
+        onCancel={() => setOpen(false)}
+        width={400}
+        footer={null}
+
+      >
+        <p>some contents...</p>
+      </Modal>
+    </>
+  );
+};
+
+export default BasicModal;
+
