ff8a155763304c4b706f89ef12f02f93b3e6bdc3
faheem4531 <faheem4531@gmail.com>
2024-05-08 22:44:00 +0500

    folder structure

--- src/app/_components/Modals/InvestmentType.tsx
+++ src/app/_components/Modals/InvestmentType.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { Button, Modal } from 'antd';
 
 import styles from "./styles.module.scss"
-import MenuBar from './manuBar/Menu';
+import MenuBar from '../manuBar/Menu';
 
 interface InvestmentTypeProps {
   onClick: () => void;
