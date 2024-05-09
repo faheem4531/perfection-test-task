5c4288e2cf70d4a03a27b51688664ae86d83f9d2
faheem4531 <faheem4531@gmail.com>
2024-05-10 00:00:19 +0500

    modal connection

--- src/app/_components/tableHeader/Header.tsx
+++ src/app/_components/tableHeader/Header.tsx
@@ -1,38 +1,48 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd';
 
 import styles from "./Header.module.scss"
 import MenuBar from '../manuBar/Menu';
 import { approval, dateTime, notDefine, changeApprovalStatus } from "@/app/utlis/constants"
+import InvestmentType from '../modals/InvestmentType';
 
 
 const TableHead = () => {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div>
-      <div className={styles.header}>
-        <h2>회원 목록 <span>(총 100명 | 승인대기 1건)</span></h2>
-        <div className={styles.headerOptions}>
-          <MenuBar menuItems={approval} width="150px" />
-          <MenuBar menuItems={dateTime} width="150px" />
-          <MenuBar menuItems={notDefine} width="150px" />
+    <>
+      <div>
+        <div className={styles.header}>
+          <h2>회원 목록 <span>(총 100명 | 승인대기 1건)</span></h2>
+          <div className={styles.headerOptions}>
+            <MenuBar menuItems={approval} width="150px" />
+            <MenuBar menuItems={dateTime} width="150px" />
+            <MenuBar menuItems={notDefine} width="150px" />
+          </div>
         </div>
-      </div>
-      <div className={styles.afterHeder}>
-        <Button className={styles.btnCheck} onClick={() => { }} >
-          등록
-        </Button>
-        <div >
-          <p>선택한 0건</p>
-          <MenuBar menuItems={changeApprovalStatus} width="150px" />
-          <Button className={styles.btnCheck} onClick={() => { }} >
+        <div className={styles.afterHeder}>
+          <Button className={styles.btnCheck} onClick={() => setOpen(true)} >
             등록
           </Button>
+          <div >
+            <p>선택한 0건</p>
+            <MenuBar menuItems={changeApprovalStatus} width="150px" />
+            <Button className={styles.btnCheck} onClick={() => { }} >
+              등록
+            </Button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <InvestmentType
+        onClick={() => { }}
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+    </>
   );
 };
 
